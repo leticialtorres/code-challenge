a53f77eef1c7905f534fc8fde0ae1e36037c2a98
thiagomart <thiagomsl@gmail.com>
2020-02-01 10:49:33 -0300

    feat: #1 home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from "@angular/core";
+
+import { CharactersResponseModel } from "./integration/response/characters-response.model";
+import { HomeService } from "./../services/home.service";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.sass"]
+})
+export class HomeComponent implements OnInit {
+  caracters: Array<CharactersResponseModel>;
+
+  constructor(private service: HomeService) {}
+
+  ngOnInit() {
+    this.getAllCaracters();
+  }
+
+  getAllCaracters(): void {
+    this.service.getAllCaracters().subscribe(res => {
+      this.caracters = res;
+    });
+  }
+}
