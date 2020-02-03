c35361f074227cebf62d861df0dc5fc0c0b84987
thiagomart <thiagomsl@gmail.com>
2020-02-03 00:19:16 -0300

    chore(home): unit test from character detail

---
+++ src/app/character-detail/test/character-detail.stub.ts
@@ -0,0 +1,34 @@
+import { Builder } from 'builder-pattern';
+import { CharacterResponseModel } from './../../home/integration/response/characters-response.model';
+import { CharacterModel } from 'src/app/home/models/character.model';
+
+
+export class CharacterDetailStub {
+
+	getAllCharacters(){};
+	getCharacterById(){};
+
+    static mockCharacterResponseModel(): Array<CharacterResponseModel> {
+		return [Builder<CharacterResponseModel>()
+		.char_id(1)
+		.img('')
+		.name('Jonh')
+		.nickname('Josua')
+		.occupation(['Reader', 'Speaker'])
+		.portrayed('Key')
+		.status('Alive')
+		.build()];
+	}
+
+	static mockCharacterModel(): CharacterModel {
+		return Builder<CharacterModel>()
+		.char_id(1)
+		.img('')
+		.name('Jonh')
+		.nickname('Josua')
+		.occupation(['Reader', 'Speaker'])
+		.portrayed('Key')
+		.status('Alive')
+		.build();
+    }
+}
