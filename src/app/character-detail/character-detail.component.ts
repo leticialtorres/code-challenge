071b10b1a3ef0e97901e54a0c64a2046493f66c2
thiagomart <thiagomsl@gmail.com>
2020-02-02 21:19:33 -0300

    chore(character-detail): updates on component and html

--- src/app/character-detail/character-detail.component.ts
+++ src/app/character-detail/character-detail.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
+import { CharacterModel } from '../home/models/character.model';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { HomeService } from '../home/services/home.service';
+import { CharacterMapper } from '../home/mapper/character.mapper';
 
 @Component({
   selector: 'app-character-detail',
@@ -6,10 +11,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./character-detail.component.sass']
 })
 export class CharacterDetailComponent implements OnInit {
+	character: CharacterModel;
 
-  constructor() { }
+	constructor(
+		private route: ActivatedRoute,
+		private service: HomeService,
+		private changeDetector: ChangeDetectorRef
+		) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.route.paramMap
+		.pipe(switchMap(params => {
+			const id = String(params.get('id'));
+			return this.service.getCharacterById(id);
+		}))
+		.subscribe(characterResponse => {
+			this.character = characterResponse;
+			this.changeDetector.detectChanges();
+		});
+	}
+
+	isDead(dead: string): string {
+		return dead;
+	}
+
+	characterDefined(): boolean {
+		return !!this.character;
+	}
 
 }
