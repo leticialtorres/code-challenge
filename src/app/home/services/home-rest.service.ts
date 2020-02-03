2f85375a1798f8c6f366dcb3b759cbd48284f78f
thiagomart <thiagomsl@gmail.com>
2020-02-02 21:23:54 -0300

    refactor(project): rename class and add getCharacterById

--- src/app/home/services/home-rest.service.ts
+++ src/app/home/services/home-rest.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 import { Observable, of } from 'rxjs';
+
 import { environment } from 'src/environments/environment';
-import { CharactersResponseModel } from '../integration/response/characters-response.model';
+import { CharacterResponseModel } from '../integration/response/characters-response.model';
 
 @Injectable({
 	providedIn: 'root'
@@ -14,8 +16,13 @@ export class HomeRestService {
 	) { }
 
 
-	getAllCaracters(): Observable<Array<CharactersResponseModel>> {
+	getAllCharacters(): Observable<Array<CharacterResponseModel>> {
 		return of({})
-		.pipe( res => this.httpClient.get<Array<CharactersResponseModel>>(`${environment.apiURL}/characters`));
+		.pipe( res => this.httpClient.get<Array<CharacterResponseModel>>(`${environment.apiURL}/characters`));
+	}
+
+	getCharactersById(id: string): Observable<Array<CharacterResponseModel>> {
+		return of({})
+		.pipe( res => this.httpClient.get<Array<CharacterResponseModel>>(`${environment.apiURL}/characters/${id}`));
 	}
 }
